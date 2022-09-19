1bb82ce85c87314bd7cee8851954c326cd2307da
Josue Diaz <joem.dima@outlook.com>
2022-09-19 13:14:14 -0500

    Initial Commit

---
+++ SnackbarBug/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SnackbarBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ SnackbarBug/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace SnackbarBug;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        //Application.Current.MainPage.DisplaySnackbar("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", duration: TimeSpan.FromSeconds(5));
+        Shell.Current.DisplaySnackbar("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", duration: TimeSpan.FromSeconds(5));
+	}
+}
